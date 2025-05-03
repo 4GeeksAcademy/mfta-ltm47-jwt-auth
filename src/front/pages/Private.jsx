01d2ccd7684ab79bdc0fcef2fcdf312e8105567f
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2025-05-03 19:05:00 +0000

    Private section

--- src/front/pages/Private.jsx
+++ src/front/pages/Private.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useGlobalReducer from '../hooks/useGlobalReducer'
+import { useNavigate } from 'react-router-dom'
 
 const Private = () => {
+  const navigate = useNavigate()
+  const { store, dispatch } = useGlobalReducer()
+  const { user } = store
+  
+  useEffect(() => {
+    // Check if the user is logged in
+    if (!user) {
+      // If not, redirect to the login page
+      navigate('/login')
+    }
+  }
+  , [user, navigate])
+
   return (
     <div>Private</div>
   )
